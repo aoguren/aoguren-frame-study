09a3c22f97a0c1338338b921d1c624e48b7c5ae9
aoguren <aoguren@126.com>
2016-09-01 09:05:39 +0800

    新增：   1、增加使用classpath方式，加载依赖注入。

---
+++ spring/src/main/java/com/glxt/study/spring/springinaction/chapter1/knight/Minstrel.java
@@ -0,0 +1,22 @@
+package com.glxt.study.spring.springinaction.chapter1.knight;
+
+import java.io.PrintStream;
+
+public class Minstrel {
+
+  private PrintStream stream;
+  
+  public Minstrel(PrintStream stream) {
+    this.stream = stream;
+  }
+
+  public void singBeforeQuest() {
+    stream.println("Fa la la, the knight is so brave!");
+  }
+
+  public void singAfterQuest() {
+    stream.println("Tee hee hee, the brave knight " +
+    		"did embark on a quest!");
+  }
+
+}
---
+++ spring/src/main/java/com/glxt/study/spring/springinaction/chapter1/knight/RescueDamselQuest.java
@@ -0,0 +1,9 @@
+package com.glxt.study.spring.springinaction.chapter1.knight;
+
+public class RescueDamselQuest implements Quest {
+
+  public void embark() {
+    System.out.println("Embarking on a quest to rescue the damsel.");
+  }
+
+}
