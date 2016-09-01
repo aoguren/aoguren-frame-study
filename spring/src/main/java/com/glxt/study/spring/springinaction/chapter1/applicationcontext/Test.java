731187719a93fe23bc6fb1f0500d34ecb95784c6
aoguren <aoguren@126.com>
2016-09-01 17:36:21 +0800

    spring容器测试 方式二：FileSystemXmlApplicationContext

--- spring/src/main/java/com/glxt/study/spring/springinaction/chapter1/applicationcontext/Test.java
+++ spring/src/main/java/com/glxt/study/spring/springinaction/chapter1/applicationcontext/Test.java
@@ -2,6 +2,7 @@ package com.glxt.study.spring.springinaction.chapter1.applicationcontext;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Test
 {
@@ -9,10 +10,24 @@ public class Test
 	{
 		System.out.println("hello world!");
 	}
+
 	public static void main(String[] args)
 	{
+		method2();
+	}
+	public static void method2()
+	{
+		// 方式二：FileSystemXmlApplicationContext
+		ApplicationContext context = new FileSystemXmlApplicationContext(
+				"E:/study/spring-study/spring/src/main/java/com/glxt/study/spring/springinaction/chapter1/applicationcontext/application.xml");
+		Test test = (Test) context.getBean("test");
+		test.say();
+	}
+	public static void method1()
+	{
 		// 方式一 ：ClassPathXmlApplicationContext
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/glxt/study/spring/springinaction/chapter1/applicationcontext/application.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/glxt/study/spring/springinaction/chapter1/applicationcontext/application.xml");
 		Test test = (Test) context.getBean("test");
 		test.say();
 	}
