daccfa1c7e038057877572d800d529ede968d00b
aoguren <aoguren@126.com>
2016-09-01 09:48:58 +0800

    新增：  1、添加AOP编程。

--- spring/src/main/java/com/glxt/study/spring/springinaction/chapter1/knight/BraveKnight.java
+++ spring/src/main/java/com/glxt/study/spring/springinaction/chapter1/knight/BraveKnight.java
@@ -3,10 +3,11 @@ package com.glxt.study.spring.springinaction.chapter1.knight;
 public class BraveKnight implements Knight {
 
   private Quest quest;
-
+  
   public BraveKnight(Quest quest) {
-    this.quest = quest;
+	    this.quest = quest;
   }
+  
 
   public void embarkOnQuest() {
     quest.embark();
