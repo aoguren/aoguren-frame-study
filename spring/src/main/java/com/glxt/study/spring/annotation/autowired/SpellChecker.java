f005df3fab1814b8f735c4c514948f83d2687976
aoguren <aoguren@126.com>
2016-09-20 10:48:29 +0800

    添加注释@Autowired使用

---
+++ spring/src/main/java/com/glxt/study/spring/annotation/autowired/SpellChecker.java
@@ -0,0 +1,11 @@
+package com.glxt.study.spring.annotation.autowired;
+
+public class SpellChecker {
+   public SpellChecker(){
+      System.out.println("Inside SpellChecker constructor." );
+   }
+   public void checkSpelling(){
+      System.out.println("Inside checkSpelling." );
+   }
+   
+}
